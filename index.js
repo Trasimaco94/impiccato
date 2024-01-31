0b078530f2aec2c29e3f69fe7ce0416058bfb00f
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2024-01-31 13:30:43 +0100

    fixed scritp

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-let wordBox = document.getElementById('wordBox');
-let input = document.getElementById('input');
-let button = document.getElementById('btn');
-let counterBox = document.getElementById('counterBox');
-let spanLifePoints = document.getElementById('lifePoints');
+let wordBox = document.getElementById("wordBox");
+let input = document.getElementById("inputId");
+let button = document.getElementById("btn");
+let counterBox = document.getElementById("counterBox");
+let spanLifePoints = document.getElementById("lifePoints");
 
-let parolaDaIndovinare = ['c', 'h', 'a', 't', 'g', 'p', 't']
-
-
+let parolaDaIndovinare = ["c", "h", "a", "t", "g", "p", "t"];
+let lettereIndovinate = [];
+let lifePoints = 6;
 
 function onClick() {
-    checkLetters();
-    checkCounter();
+  checkLetters();
+  checkCounter();
 }
 
 function checkLetters() {
-    let valore = input.value;
-    parolaDaIndovinare.forEach(() => {
-        if (parolaDaIndovinare.includes(valore)) {
-            parolaDaIndovinare.style.display = 'inline-block';
-
-        }
-    });
-
-
-
-
-};
-
-function checkCounter() {
-
+  let valore = input.value.toLowerCase();
+  if (parolaDaIndovinare.includes(valore)) {
+    if (!lettereIndovinate.includes(valore)) {
+      lettereIndovinate.push(valore);
+      updateWordBox();
+    }
+  }
 }
 
-button.addEventListener('click', onClick)
+function updateWordBox() {
+  wordBox.textContent = parolaDaIndovinare
+    .map((lettera) => (lettereIndovinate.includes(lettera) ? lettera : "_"))
+    .join(" ");
+}
+
+function checkCounter() {
+  if (!parolaDaIndovinare.includes(input.value.toLowerCase())) {
+    lifePoints--;
+    spanLifePoints.textContent = lifePoints;
+  }
+
+  if (lifePoints === 0) {
+    alert("Hai perso! La parola era: ChatGPT! ");
+    resetGame();
+  }
+
+  if (lettereIndovinate.length === parolaDaIndovinare.length) {
+    alert("Complimenti! Hai indovinato la parola!");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  lettereIndovinate = [];
+  lifePoints = 6;
+  spanLifePoints.textContent = lifePoints;
+  input.value = "";
+  updateWordBox();
+}
+
+button.addEventListener("click", onClick);
